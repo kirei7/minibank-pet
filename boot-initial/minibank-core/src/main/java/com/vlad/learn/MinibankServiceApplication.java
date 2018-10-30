80c664c95e3ff7deeb68c4048ee9373916281cfd
Vladyslav Sereda <vladyslav_sereda1@epam.com>
2018-10-30 16:50:44 +0200

    Initial commit

---
+++ boot-initial/minibank-core/src/main/java/com/vlad/learn/MinibankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vlad.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MinibankServiceApplication.class, args);
+    }
+}
